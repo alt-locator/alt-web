2660729c6d0703d5b5298844714f1206eba1fa67
angular-cli <angular-cli@angular.io>
2016-07-31 21:13:22 -0700

    chore: initial commit from angular-cli                           _                      _ _    __ _ _ __   __ _ _   _| | __ _ _ __       ___| (_)   / _  |  _ \ / _  | | | | |/ _  |  __|____ / __| | |  | (_| | | | | (_| | |_| | | (_| | | |_____| (__| | |   \____|_| |_|\__  |\____|_|\____|_|        \___|_|_|               |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AltWebPage } from './app.po';
+
+describe('alt-web App', function() {
+  let page: AltWebPage;
+
+  beforeEach(() => {
+    page = new AltWebPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
