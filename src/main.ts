2bc2dae3b35720a936a58e1fe447eeee394e2422
Craig Nishina <craig.nishina@gmail.com>
2016-07-31 22:10:30 -0700

    chore(firebase): add firebase components

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,27 @@
-import { bootstrap } from '@angular/platform-browser-dynamic';
-import { enableProdMode } from '@angular/core';
+import {bootstrap} from '@angular/platform-browser-dynamic';
+import {enableProdMode} from '@angular/core';
+import {ROUTER_PROVIDERS} from '@angular/router';
+import {
+  FIREBASE_PROVIDERS,
+  defaultFirebase,
+  firebaseAuthConfig,
+  AuthMethods,
+  AuthProviders
+} from 'angularfire2';
+
 import { AltWebAppComponent, environment } from './app/';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(AltWebAppComponent);
+bootstrap(AltWebAppComponent, [
+  ROUTER_PROVIDERS,
+  FIREBASE_PROVIDERS,
+  defaultFirebase({
+    apiKey: '',
+    authDomain: '',
+    databaseURL: 'alt-github.firebaseio.com',
+    storageBucket: ''
+  })
+]);
